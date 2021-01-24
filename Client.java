ded5876e1866ce470ef3fbef3fe9b8ef60870789
isaccanedo <isaccanedo@gmail.com>
2021-01-24 14:52:57 -0300

    Initial commit

---
+++ Client.java
@@ -0,0 +1,34 @@
+package sockets;
+
+import java.io.*;
+import java.net.*;
+
+/**
+ *
+ * @author Isac Canedo
+ */
+ 
+public class Client {
+
+    public static void main(String arg[]) throws IOException, ClassNotFoundException {
+
+        int portNum = 11113;
+
+        Socket socket = new Socket("localhost", portNum);
+
+        // Integer Object to send to Server.
+        Integer num = new Integer(50);
+
+        ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+        ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+        
+        out.writeObject(num);
+                
+        String response = (String) in.readObject();
+
+        System.out.println("Server message: " + response);
+        
+        //socket.close();
+        
+    }
+}
